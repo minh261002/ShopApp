8fff88b467f1b649372eaab0a135f2fcf324a506
minhttran <trcongminh.261002@gmail.com>
2025-01-20 13:07:16 +0700

    add RepositoryServiceProvider and ServiceServiceProvider; update AppServiceProvider and providers list

---
+++ app/Admin/Providers/ServiceServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Admin\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ServiceServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
