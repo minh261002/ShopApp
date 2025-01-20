2f7049d0e38ce04d5643d2172db21cb6d4a39cb6
minhttran <trcongminh.261002@gmail.com>
2025-01-20 12:37:59 +0700

    add Post, PostCatalogue, Slider, and SliderItem models with relationships and soft deletes; introduce ActiveStatus enum for status management

--- app/Models/Slider.php
+++ app/Models/Slider.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use App\Enums\Module\ActiveStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Slider extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'sliders';
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'status' => ActiveStatus::class
+    ];
+
+    public function items()
+    {
+        return $this->hasMany(SliderItem::class);
+    }
 }
--- app/Models/SliderItem.php
+++ app/Models/SliderItem.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SliderItem extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'slider_items';
+
+    protected $guarded = [];
+
+    public function slider()
+    {
+        return $this->belongsTo(Slider::class);
+    }
 }
