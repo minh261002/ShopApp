bee078890b4b7c6a22c96d327ea57966d9ea0e1a
minhttran <trcongminh.261002@gmail.com>
2025-01-20 14:54:09 +0700

    add sliders and slider_items tables; define schema with necessary fields and relationships

--- database/migrations/2025_01_20_122603_create_slider_items_table.php
+++ database/migrations/2025_01_20_122603_create_slider_items_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,17 @@ return new class extends Migration
     {
         Schema::create('slider_items', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('slider_id');
+            $table->string('title');
+            $table->string('sub_title')->nullable();
+            $table->string('button_text')->nullable();
+            $table->text('link')->nullable();
+            $table->tinyInteger('position');
+            $table->text('image');
+            $table->text('mobile_image')->nullable();
             $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('slider_id')->references('id')->on('sliders')->onDelete('cascade');
         });
     }
 
