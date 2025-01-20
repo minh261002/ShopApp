bee24b76b888e1114098af33650228e0b9eb9127
minhttran <trcongminh.261002@gmail.com>
2025-01-20 15:07:46 +0700

    add post and post catalogue management views; implement image display, status toggle, and action modals

--- database/migrations/2025_01_20_122518_create_post_catalogues_table.php
+++ database/migrations/2025_01_20_122518_create_post_catalogues_table.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Enums\ActiveStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +13,26 @@ return new class extends Migration
     {
         Schema::create('post_catalogues', function (Blueprint $table) {
             $table->id();
+            $table->integer('_lft');
+            $table->integer('_rgt');
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->string('name');
+            $table->string('image')->nullable();
+            $table->string('slug')->unique();
+            $table->integer('position')->default(0);
+            $table->enum('status', ActiveStatus::getValues())->default(ActiveStatus::Active->value);
+            $table->boolean('show_menu')->default(false);
+            $table->longText('desc')->nullable();
+            $table->string('meta_title')->nullable();
+            $table->string('meta_description')->nullable();
+            $table->string('meta_keywords')->nullable();
             $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('parent_id')
+                ->references('id')
+                ->on('post_catalogues')
+                ->onUpdate('NO ACTION')
+                ->onDelete('SET NULL');
         });
     }
 
