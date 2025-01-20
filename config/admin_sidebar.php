703845616a36c12570494d53bf8c190fe22f75d4
minhttran <trcongminh.261002@gmail.com>
2025-01-20 12:57:57 +0700

    add DataTables configuration files and update .gitignore to exclude public directory

---
+++ config/admin_sidebar.php
@@ -0,0 +1,193 @@
+<?php
+
+return [
+    [
+        'active' => ['admin.category.*'],
+        'show' => ['admin.category.*'],
+        'title' => 'Quản lý danh mục',
+        'icon' => 'ti ti-category-2 fs-2',
+        'permission' => ['viewCategory', 'createCategory', 'editCategory', 'deleteCategory'],
+        'children' => [
+            [
+                'title' => 'Thêm mới',
+                'route' => 'admin.category.create',
+                'icon' => 'ti ti-plus fs-3 me-2',
+                'permission' => 'createCategory'
+            ],
+            [
+                'title' => 'Danh sách',
+                'route' => 'admin.category.index',
+                'icon' => 'ti ti-list fs-3 me-2',
+                'permission' => 'viewCategory'
+            ]
+        ]
+    ],
+    [
+        'active' => ['admin.post_catalogue.*'],
+        'show' => ['admin.post_catalogue.*'],
+        'title' => 'Quản lý chuyên mục',
+        'icon' => 'ti ti-list-letters fs-2',
+        'permission' => ['viewPostCatalogue', 'createPostCatalogue', 'editPostCatalogue', 'deletePostCatalogue'],
+        'children' => [
+            [
+                'title' => 'Thêm mới',
+                'route' => 'admin.post_catalogue.create',
+                'icon' => 'ti ti-plus fs-3 me-2',
+                'permission' => 'createPostCatalogue'
+            ],
+            [
+                'title' => 'Danh sách',
+                'route' => 'admin.post_catalogue.index',
+                'icon' => 'ti ti-list fs-3 me-2',
+                'permission' => 'viewPostCatalogue'
+            ]
+        ]
+    ],
+    [
+        'active' => ['admin.post.*'],
+        'show' => ['admin.post.*'],
+        'title' => 'Quản lý bài viết',
+        'icon' => 'ti ti-inbox fs-2',
+        'permission' => ['viewPost', 'createPost', 'editPost', 'deletePost'],
+        'children' => [
+            [
+                'title' => 'Thêm mới',
+                'route' => 'admin.post.create',
+                'icon' => 'ti ti-plus fs-3 me-2',
+                'permission' => 'createPost'
+            ],
+            [
+                'title' => 'Danh sách',
+                'route' => 'admin.post.index',
+                'icon' => 'ti ti-list fs-3 me-2',
+                'permission' => 'viewPost'
+            ]
+        ]
+    ],
+    [
+        'active' => ['admin.slider.*'],
+        'show' => ['admin.slider.*'],
+        'title' => 'Quản lý slider',
+        'icon' => 'ti ti-library-photo fs-2',
+        'permission' => ['viewSlider', 'createSlider', 'editSlider', 'deleteSlider'],
+        'children' => [
+            [
+                'title' => 'Thêm mới',
+                'route' => 'admin.slider.create',
+                'icon' => 'ti ti-plus fs-3 me-2',
+                'permission' => 'createSlider'
+            ],
+            [
+                'title' => 'Danh sách',
+                'route' => 'admin.slider.index',
+                'icon' => 'ti ti-list fs-3 me-2',
+                'permission' => 'viewSlider'
+            ]
+        ]
+    ],
+    [
+        'active' => ['admin.user.*'],
+        'show' => ['admin.user.*'],
+        'title' => 'Quản lý khách hàng',
+        'icon' => 'ti ti-user fs-2',
+        'permission' => ['viewUser', 'createUser', 'editUser', 'deleteUser'],
+        'children' => [
+            [
+                'title' => 'Thêm mới',
+                'route' => 'admin.user.create',
+                'icon' => 'ti ti-plus fs-3 me-2',
+                'permission' => 'createUser'
+            ],
+            [
+                'title' => 'Danh sách',
+                'route' => 'admin.user.index',
+                'icon' => 'ti ti-list fs-3 me-2',
+                'permission' => 'viewUser'
+            ]
+        ]
+    ],
+    [
+        'active' => ['admin.admin.*'],
+        'show' => ['admin.admin.*'],
+        'title' => 'Quản trị viên',
+        'icon' => 'ti ti-user-shield fs-2',
+        'permission' => ['viewAdmin', 'createAdmin', 'editAdmin', 'deleteAdmin'],
+        'children' => [
+            [
+                'title' => 'Thêm mới',
+                'route' => 'admin.admin.create',
+                'icon' => 'ti ti-plus fs-3 me-2',
+                'permission' => 'createAdmin'
+            ],
+            [
+                'title' => 'Danh sách',
+                'route' => 'admin.admin.index',
+                'icon' => 'ti ti-list fs-3 me-2',
+                'permission' => 'viewAdmin'
+            ]
+        ]
+    ],
+    [
+        'active' => ['admin.role.*'],
+        'show' => ['admin.role.*'],
+        'title' => 'Quản lý vai trò',
+        'icon' => 'ti ti-code fs-2',
+        'permission' => ['viewRole', 'createRole', 'editRole', 'deleteRole'],
+        'children' => [
+            [
+                'title' => 'Thêm mới',
+                'route' => 'admin.role.create',
+                'icon' => 'ti ti-plus fs-3 me-2',
+                'permission' => 'createRole'
+            ],
+            [
+                'title' => 'Danh sách',
+                'route' => 'admin.role.index',
+                'icon' => 'ti ti-list fs-3 me-2',
+                'permission' => 'viewRole'
+            ]
+        ]
+    ],
+    [
+        'active' => ['admin.permission.*'],
+        'show' => ['admin.permission.*'],
+        'title' => 'Quản lý quyền',
+        'icon' => 'ti ti-code fs-2',
+        'permission' => ['viewPermission', 'createPermission', 'editPermission', 'deletePermission'],
+        'children' => [
+            [
+                'title' => 'Thêm mới',
+                'route' => 'admin.permission.create',
+                'icon' => 'ti ti-plus fs-3 me-2',
+                'permission' => 'createPermission'
+            ],
+            [
+                'title' => 'Danh sách',
+                'route' => 'admin.permission.index',
+                'icon' => 'ti ti-list fs-3 me-2',
+                'permission' => 'viewPermission'
+            ]
+        ],
+    ],
+    [
+        'active' => ['admin.module.*'],
+        'show' => ['admin.module.*'],
+        'title' => 'Quản lý module',
+        'icon' => 'ti ti-code fs-2',
+        'permission' => ['viewModule', 'createModule', 'editModule', 'deleteModule'],
+        'children' => [
+            [
+                'title' => 'Thêm mới',
+                'route' => 'admin.module.create',
+                'icon' => 'ti ti-plus fs-3 me-2',
+                'permission' => 'createModule'
+            ],
+            [
+                'title' => 'Danh sách',
+                'route' => 'admin.module.index',
+                'icon' => 'ti ti-list fs-3 me-2',
+                'permission' => 'viewModule'
+            ]
+        ]
+    ]
+];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Admin;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Admin::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+            'image' => '/admin/images/not-found.jpg'
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            ProvinceSeeder::class,
+            DistrictSeeder::class,
+            WardSeeder::class,
+            ModuleSeeder::class
         ]);
     }
 }
