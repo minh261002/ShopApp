c6ad2b29df19fccd7a6fa7b13dffd8b8a37df577
minhttran <trcongminh.261002@gmail.com>
2025-01-21 09:58:13 +0700

    add migrations for categories and products tables; include foreign key constraints and status enums

--- database/migrations/2025_01_20_122644_create_products_categories_table.php
+++ database/migrations/2025_01_20_122644_create_products_categories_table.php
@@ -4,16 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('products_categories', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('category_id');
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+
+            $table->primary(['product_id', 'category_id']);
         });
     }
 
