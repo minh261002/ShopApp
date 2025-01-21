6235dfc8cba7745b6b3ecea06ac730768333ddfc
minhttran <trcongminh.261002@gmail.com>
2025-01-21 16:59:27 +0700

    add order and transaction models; implement relationships, factories, and guarded properties for orders, order items, order shipping, and transactions

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class OrderItem extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'order_items';
+
+    protected $guarded = [];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function productVariation()
+    {
+        return $this->belongsTo(ProductVariation::class);
+    }
 }
