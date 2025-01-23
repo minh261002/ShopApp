b5af2dffa0e687fa954e81901d5ae4917d6b2b3e
minhttran <trcongminh.261002@gmail.com>
2025-01-23 16:03:01 +0700

    feat: enhance product info layout with size guide modal and quantity selector

--- resources/views/client/product/components/box-info.blade.php
+++ resources/views/client/product/components/box-info.blade.php
@@ -1,6 +1,6 @@
 <div class="w-100">
-    <div class="d-flex flex-column">
-        <h1 class="fs-28px fw-bold text-dark">{{ $product->name }}</h1>
+    <div class="d-flex flex-column gap-3">
+        <h1 class="fs-28px mb-0 fw-bold text-dark">{{ $product->name }}</h1>
 
         <div class="d-flex align-items-center justify-content-start gap-2">
             <div class="d-flex align-items-center justify-content-start gap-1">
@@ -11,10 +11,10 @@
             <span>(11 Đánh giá)</span>
         </div>
 
-        <div class="d-flex align-items-center justify-content-between mt-3">
+        <div class="d-flex align-items-center justify-content-between">
             @if ($product->flashSale)
                 <div class="card w-100">
-                    <div class="card-header bg-red-lt text-white py-3">
+                    <div class="card-header bg-red-lt text-white py-2">
                         <h4 class="mb-0 text-center">
                             {{ $product->flashSale->first()->title }}
                         </h4>
@@ -65,7 +65,7 @@
             @endif
         </div>
 
-        <div class="d-flex align-items-center justify-content-between mt-3">
+        <div class="d-flex align-items-center justify-content-between">
             <div class="d-flex align-items-center gap-2">
                 @if ($product->flashSale)
                     @if ($product->variations->first()->sale_price)
@@ -101,7 +101,7 @@
             </div>
         </div>
 
-        <div class="d-flex align-items-center justify-content-between mt-3 gap-4">
+        <div class="d-flex align-items-center justify-content-between gap-4">
             @foreach ($groupedAttributes as $attributeName => $values)
                 <div class="flex-grow-1">
                     <label class="form-label fw-medium">{{ $attributeName }}</label>
@@ -112,6 +112,44 @@
                     </select>
                 </div>
             @endforeach
+        </div>
+
+        <div>
+            <a href="#" class="nav-link" data-bs-toggle="modal" data-bs-target="#modal-simple">
+                <i class="ti ti-ruler-2 fs-18px me-1"></i>
+                Hướng dẫn chọn kích thước
+            </a>
+        </div>
+
+        <div style="width: 160px">
+            <label class="form-label fw-medium">Số lượng</label>
+            <div class="d-flex align-items-center position-relative">
+                <button class="btn bg-red text-white position-absolute py-2 ms-1">
+                    <i class="ti ti-minus"></i>
+                </button>
+                <input type="number" class="form-control text-center" value="1" min="1" step="1"
+                    readonly>
+                <button class="btn bg-red text-white position-absolute py-2 end-0 me-1">
+                    <i class="ti ti-plus"></i>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal modal-blur fade" id="modal-simple" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Hướng dẫn chọn kích thước</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <img src="{{ asset('uploads/images/17341036337427596.webp') }}" class="w-100" />
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn me-auto" data-bs-dismiss="modal">Đóng</button>
+            </div>
         </div>
     </div>
 </div>
